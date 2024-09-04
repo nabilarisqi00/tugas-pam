524f4e0328d0df752421e4e78df2611776e5f214
nabilarisqi00 <nabilarisqir@gmail.com>
2024-09-04 07:02:02 +0700

    tugas 2 PAM

--- app/src/main/java/com/example/pam_ub/MainActivity.kt
+++ app/src/main/java/com/example/pam_ub/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.pam_ub
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+
+        val editText1=findViewById<EditText>(R.id.editText1)
+        val editText2=findViewById<EditText>(R.id.editText2)
+        val radioGroup=findViewById<RadioGroup>(R.id.radiogrup)
+        val buttonCalculate = findViewById<Button>(R.id.button)
+
+        buttonCalculate.setOnClickListener {
+            val num1 = editText1.text.toString().toDoubleOrNull()
+            val num2 = editText2.text.toString().toDoubleOrNull()
+
+            if (num1 == null || num2 == null) {
+                Toast.makeText(this, "Masukkan angka yang valid", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val result = when (radioGroup.checkedRadioButtonId) {
+                R.id.radioButtonTambah -> num1 + num2
+                R.id.radioButtonKurang -> num1 - num2
+                R.id.radioButtonKali -> num1 * num2
+                R.id.radioButtonBagi -> {
+                    if (num2 == 0.0) {
+                        Toast.makeText(this, "Tidak bisa membagi dengan nol", Toast.LENGTH_SHORT).show()
+                        return@setOnClickListener
+                    }
+                    num1 / num2
+                }
+                else -> {
+                    Toast.makeText(this, "Pilih operasi", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+            }
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("RESULT", result)
+            }
+            startActivity(intent)
+        }
+
+        }
 }
